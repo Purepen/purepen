36ace2fcfa0498137aee796d0654f64abdd86e81
Tesimune <tesimune@gmail.com>
2024-01-02 20:10:25 +0100

    Updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,20 +7,30 @@ function Projects() {
           {projectsArray.map((project) => (
             <div key={project.id} className="bg-white shadow-xl">
               <div className="border-b border-slate-900">
-                <img
-                  src={project.image}
-                  className="h-80 w-full object-cover"
-                />
+                <img src={project.image} className="h-80 w-full object-cover" />
               </div>
               <div className="flex flex-col gap-3 p-5">
-                <h1 className="py-1 px-2 text-md font-semibold">
+                <h1 className="h-9 py-1 px-2 text-md font-semibold">
                   {project.title}
                 </h1>
-                <span className="py-1 px-2 text-sm">
-                  {project.description}
+                <span className="h-20 py-1 px-2 text-sm">
+                  {project.description.slice(0, 150)},
+                  <a href={project.readMore} className="italic border-b">
+                    {' '}
+                    Learn more..
+                  </a>
                 </span>
-                <a className="py-3 px-5 border-2 rounded-md" href={project.slug}>
+                <a
+                  className="py-3 px-5 border-2 rounded-md"
+                  href={project.slug}
+                >
                   VIEW PROJECT
+                </a>
+                <a
+                  className="py-3 px-5 border-2 rounded-md"
+                  href={project.file}
+                >
+                  VIEW PROJECT FILE
                 </a>
               </div>
             </div>
