bb99a3bd647a14a7bc87a485fd08589c05a23f2a
Purepen <66730885+Purepen@users.noreply.github.com>
2024-01-04 18:23:46 +0100

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,7 +7,7 @@ function Resume() {
         <div className="container mx-auto py-16 md:px-20 px-5">
           <div className="grid gap-2 w-full">
             <label className="font-bold text-3xl">Resume</label>
-            <a href="https://tinyurl.com/myresumepurepen" target="blank" className="text-lg">Download my Resume Now</a>
+            <a href="https://drive.google.com/file/d/1a3cBMuvR6yhWiZuMkkn3-B3tRBdcaU3B/view?usp=drive_link" target="blank" className="text-lg">Download my Resume Now</a>
           </div>
         </div>
       </section>
